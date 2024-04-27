d3ed8bf6825431ba152cc6eb9fcad1ef2310033c
vijaykumardumpeti <pavanreddyr34@gmail.com>
2024-04-27 17:25:50 +0530

    cgs

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import morgan from "morgan";
 import connectDb from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
@@ -17,7 +16,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
