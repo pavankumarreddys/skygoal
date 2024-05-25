373aff0c571407ca3a82923b4fd417106259d15f
Pavan Kumar Reddy Sannala <126879065+pavankumarreddys@users.noreply.github.com>
2024-05-25 09:47:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Connected to MongoDB database ${conn.connection.host}`);
   } catch (error) {
-    console.log("Error:", error);
+    console.log("Error:", error)
   }
 };
 
